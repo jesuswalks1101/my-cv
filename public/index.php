db4225a237be61b469f9aad1912ced7a6455683e
Adrian URSU <ursuadrian19@gmail.com>
2021-06-03 18:27:45 +0300

    Changed .html in .php and hosts

--- public/index.php
+++ public/index.php
@@ -5,9 +5,11 @@
 </head>
 <body>
 
-	<h1>Adrian URSU ...</h1>
+	<h1>Adrian URSU</h1>
 	<p>Email: ursuadrian19@gmail.com</p>
 	<div>Telefon: 078754706</div>
-
+	<div>
+        <?php echo "Today is " . date("Y/m/d H:i:s"); ?>
+    </div>
 </body>
 </html>
