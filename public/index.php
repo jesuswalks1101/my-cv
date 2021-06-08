3e8bfdd167f06ae2991b37ee00efbb8c1d067f13
Adrian URSU <ursuadrian19@gmail.com>
2021-06-08 18:00:20 +0300

    Updatet index.php

--- public/index.php
+++ public/index.php
@@ -1,15 +1,111 @@
 <!DOCTYPE html>
 <html>
 <head>
+<meta>
 <title>Curriculum Vitae of Adrian URSU</title>
 </head>
 <body>
 
-	<h1>Adrian URSU</h1>
+	<!-- <h1>Adrian URSU</h1>
 	<p>Email: ursuadrian19@gmail.com</p>
 	<div>Telefon: 078754706</div>
 	<div>
         <?php echo "Today is " . date("Y/m/d H:i:s"); ?>
-    </div>
+    </div> -->
+	<header>
+		<h1>Portofolio</h1>
+		<nav>
+		<ul>
+		<li><a href="#about">About</a></li>
+		<li><a href="#skills">Skills</a></li>
+		<li><a href="#experience">Experience</a></li>
+		<li><a href="#contact">Contact</a></li>
+		</ul>
+		</nav>
+	</header>
+
+	<main>
+	<section id="about">
+		<h2>About</h2>
+		<img src="https://avatarfiles.alphacoders.com/495/thumb-49573.jpg" alt="Avatar" width="150">
+		<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim necessitatibus ducimus ipsam officia iusto quisquam porro? Quasi amet aspernatur minima deleniti, velit et nostrum doloribus.</p>
+		
+	</section>
+
+	<section id="skills">
+		<h2>Skills</h2>
+		<ul>
+		<li>HTML</li>
+		<li>CSS</li>
+		<li>JS</li>
+		<li>PHP</li>
+		<li>Laravel</li>
+		<li>Bootstrap</li>
+		</ul>
+	</section>
+
+	<section id="experience">
+		<h2>Experience</h2>
+		<table>
+			<thead>
+				<tr>
+					<th>Period</th>
+					<th>Place</th>
+					<th>Positon/Deiscription</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>2018-present</td>
+					<td>Developmentis</td>
+					<td>
+					
+					<section>
+					
+					<h3>PHP developer</h3>
+					<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed, reiciendis?</p>
+					</section>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	</section>
+
+	<section id="contact">
+		<h2>Contact</h2>
+		<ul>
+		<li>Tel: <a href="tel+121212121">+22 121 212 1/</a></li>
+		<li>Email: uasdu@asdasd.com</li>
+		<li>Adress: str Bornks 10/564</li>
+		<li>Facebook: Johan Panamera</li>
+		</ul>
+	</section>
+	</main>
+
+	<footer>
+		<div align="center">
+		Copyright &copy; 2021, DevelopmentAID
+		</div>
+	</footer>
+
 </body>
 </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
